4424f2b9c60a03abbd8ce1609ff17b4f575a81e8
Robert He <2567466856@qq.com>
2024-04-10 12:29:13 +0800

    v1.0.0
    A relatively stable version.

--- FrzVoid.py
+++ FrzVoid.py
@@ -1,14 +1,15 @@
 import os
 import sys
 import time
-from tkinter import ttk, Tk
+import tkinter.font as tk_font
+from mttkinter.mtTkinter import Tk, Menu, BooleanVar
+from tkinter import ttk
+from subprocess import (Popen, PIPE, CREATE_NO_WINDOW)
 from threading import Thread
-
-
 from win32gui import FindWindow, ShowWindow
 from psutil import pids, Process
 
-DEFAULT_VOID = 'DFStd.exe,DFEnt.exe,install.exe,Freeze,Deep,冰点,还原,精灵,360,安全卫士,电脑管家,鲁大师'
+DEFAULT_VOID = 'a bad software\'s name|a part of another evil app\'s name'
 FRZ_DATA = 'C:/ProgramData/Frzvoid/data.ini'
 FRZ_DATA_PATH = 'C:/ProgramData/Frzvoid'
 
@@ -28,65 +29,78 @@ class CheckFVProgress:
             try:
                 ShowWindow(self.hwnd, 5)
                 return True
-            except:
+            except Exception as e:
+                print(e)
                 return False
         return False
 
 
-"""
-def WPDProgressRunning():
-    if FindWindow(None, 'WPDefender'):
-        return True
-    return False
-"""
+def turn_schedule(state: bool, result_var: BooleanVar):
+    add_scd_cmd = f'schtasks /create /tn FrzVoidStartup /tr "{sys.argv[0]} --startup_visit" /sc ONLOGON /rl highest /f'
+    del_sch_cmd = f'schtasks /delete /tn FrzVoidStartup /f'
+    cmd = add_scd_cmd if state else del_sch_cmd
+    p = Popen(cmd, stdin=PIPE, creationflags=CREATE_NO_WINDOW)
+    p.communicate(input=b'y\n')
+    result_var.set(get_startup_state())
+    # p.stdin.write(b'y\n')
+    # os.system(del_sch_cmd)
+    # if state:
+    #     os.system(add_scd_cmd)
+    time.sleep(1)
 
 
 def get_startup_state():
-    if "--startup_visit" in sys.argv:
+    chk_cmd = f'schtasks /query /tn FrzVoidStartup'
+    return bool("".join(os.popen(chk_cmd).readlines()))
+
+
+def is_startup():
+    if "--startup_visit" in sys.argv or "--wpd" in sys.argv:
         return True
     return False
 
 
-def writeData(name):
+def write_data(name):
     global features
-    features = name.split(',')
+    features = name.split('|')
     if not os.path.exists(FRZ_DATA_PATH):
         os.makedirs(FRZ_DATA_PATH)
     with open(FRZ_DATA, 'w') as f:
         f.write(name)
 
 
-def getData():
+def get_data():
     with open(FRZ_DATA, 'r') as f:
         data = f.readlines()
+        if not data:
+            return ""
         name = data[0]
         return name
 
 
-def readData():
-    return getData().split(',')
+def read_data():
+    return get_data().split('|')
 
 
-def startFreeze():
+def start_freeze():
     previous_pids = []
     while continueKilling:
-        if not FindWindow(None, 'FrzVoid'):
-            break
-        previous_pids = killFreeze(
-            nameList=features, previousPids=previous_pids)
+        # if not FindWindow(None, 'FrzVoid'):
+        #     break
+        # print('Running')
+        previous_pids = kill_freeze(
+            name_list=features, previous_pids=previous_pids)
         time.sleep(1)
 
 
-def killFreeze(nameList, previousPids):
-
+def kill_freeze(name_list, previous_pids):
     current_pids = pids()
     for a_pid in current_pids:
-        if a_pid in previousPids:
+        if a_pid in previous_pids:
             continue
         try:
             p = Process(a_pid)
-            # or feature in p.parent().name()
-            if any(feature != '' and feature in p.name() for feature in nameList):
+            if any(feature != '' and feature in p.name() for feature in name_list):
                 print('pid-%s,pname-%s' % (p.pid, p.name()))
                 time.sleep(0.5)
                 cmd = f'start /B tskill {p.pid}'
@@ -97,12 +111,12 @@ def killFreeze(nameList, previousPids):
     return current_pids
 
 
-def mk_ui(hideRoot):
-
-    def save_close():
-        root.withdraw()
-        name = targetNameEntry.get()
-        writeData(name)
+def mk_ui(hide_root):
+    def save_data():
+        name = target_name_entry.get()
+        write_data(name)
+        save_btn.config(text="Success!")
+        root.after(1000, lambda: save_btn.config(text="Save"))
 
     def exit_program():
         global continueKilling
@@ -111,45 +125,58 @@ def mk_ui(hideRoot):
         root.destroy()
 
     root = Tk()
-    if hideRoot:
+    if hide_root:
         root.withdraw()
-    root.geometry('365x115')
+    root.geometry('465x135')
     root.title('FrzVoid')
+    root.iconbitmap(os.path.join(sys._MEIPASS, 'nmico.ico'))
     root.protocol('WM_DELETE_WINDOW', root.withdraw)
-    targetNameLabel = ttk.Label(root, text='Target-name:')
-    targetNameLabel.grid(row=0, column=0, padx=10, pady=5, sticky='NW')
-    targetNameEntry = ttk.Entry(root)
-    targetNameEntry.grid(row=1, column=0, padx=10, pady=0,
-                         ipadx=100, sticky='NW')
-    targetNameEntry.insert(0, getData())
-    noticeLabel = ttk.Label(
-        root, text='Notice: Enter target-name, split by ","')
-    noticeLabel.grid(row=2, column=0, padx=10, pady=0, sticky='NW')
-    closeBtn = ttk.Button(root, text='Exit', command=exit_program)
-    closeBtn.grid(row=3, column=0, padx=10, ipadx=25, pady=5, sticky='NW')
-    saveBtn = ttk.Button(root, text='Save', command=save_close)
-    saveBtn.grid(row=3, column=0, padx=220, ipadx=25, pady=5, sticky='NW')
-    root.bind('<Return>', save_close)
+    tkfont = tk_font.nametofont("TkDefaultFont")
+    tkfont.config(family='Microsoft YaHei UI')
+    root.option_add("*Font", tkfont)
+    target_name_label = ttk.Label(root, text='Target-name:')
+    target_name_label.grid(row=0, column=0, padx=10, pady=5, sticky='NSEW')
+    target_name_entry = ttk.Entry(root)
+    target_name_entry.grid(row=1, column=0, columnspan=2, padx=10, pady=0,
+                           ipadx=100, sticky='NSEW')
+    target_name_entry.insert(0, get_data())
+    notice_label = ttk.Label(
+        root, text='Notice: Enter target-name, split by "|"')
+    notice_label.grid(row=2, column=0, padx=10, pady=0, sticky='NSEW')
+    close_btn = ttk.Button(root, text='Exit', command=exit_program)
+    close_btn.grid(row=3, column=0, padx=10, ipadx=5, pady=5, sticky='NSEW')
+    save_btn = ttk.Button(root, text='Save', command=save_data)
+    save_btn.grid(row=3, column=1, padx=10, ipadx=25, pady=5, sticky='NSEW')
+    root.bind('<Return>', save_data)
+    root.grid_columnconfigure(1, weight=1, minsize=200)
+
+    main_menu = Menu(root)
+    option_menu = Menu(main_menu, tearoff=False)
+    is_startup_set = BooleanVar(value=get_startup_state())
+    option_menu.add_checkbutton(label="Run on system Startup", variable=is_startup_set,
+                                command=lambda: Thread(turn_schedule(is_startup_set.get(), is_startup_set)).start())
+    main_menu.add_cascade(label="Options", menu=option_menu)
+    root.config(menu=main_menu)
     root.mainloop()
 
 
 def main():
     global features, continueKilling
     if not os.path.exists(FRZ_DATA):
-        writeData(DEFAULT_VOID)
-    features = readData()
+        write_data(DEFAULT_VOID)
+    features = read_data()
     continueKilling = True
     checkpgs_result = CheckFVProgress()
     time.sleep(0.5)
     if not checkpgs_result.continue_this_progress:
         return
 
-    hideRoot = False
-    if get_startup_state():
-        hideRoot = True
-    startFreeze_thread = Thread(target=startFreeze, daemon=True)
-    startFreeze_thread.start()
-    mk_ui(hideRoot=hideRoot)
+    hide_root = False
+    if is_startup():
+        hide_root = True
+    start_freeze_thread = Thread(target=start_freeze, daemon=True)
+    start_freeze_thread.start()
+    mk_ui(hide_root=hide_root)
 
 
 if __name__ == '__main__':
--- README.md
+++ README.md
@@ -1,29 +1,17 @@
 # FrzVoid
- A Windows app enabling you to prevent sepcific softwares from running
+A Windows app enabling you to prevent specific programmes from running
 
 ## Instructions
 
-### FrzVoid.exe
+If this is running, it continuously prevents some pieces of software from running for more than 1 sec since it's started.
 
-If this is running, it continuously prevents some softwares from running for more than 3 sec since it's started.
+You can add some apps-to-kill through editing the names of their .exe files in the textbox of the main root, split by '|' .
 
-You can edit the names of the softwares to kill in the textbox of it's main root, split by ','
+You can also start it upon Windows startup.
 
-### Windows Process Defender.exe
+## Furthermore...
 
-This is a tool made to help you keep the FrzVoid.exe continuously running and making it unstopable.
+Using this, you can prevent your naughty kids from installing any weird app or game on your beloved PC
+and making it messy, as long as you know their names ;)
 
-As the moment you run it for the first time, it will set itself to start with windows
-
-When it's running, if it finds that the FrzVoid.exe isn't running, it will start it immediately.
-
-## Further more...
-
-This app ISN'T STABLE accroding to long term tests, and there's still a lot of things that can be improved.
-
-It's written for fun, and to test if a function in my mind is feasible, and the answer is yes, actually (bullshit)
-
-Using this, you can prevent your naughty kids from installing any werid app or game on your beloved PC,
-and making it messy after all ;)
-
-If I get some more spare time, I will optimise this carefully, or you can do this as well!
+This is a simple program though, but enjoy it!
